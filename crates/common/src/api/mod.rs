8d28e836ccd16c1b60e3ffa606eb6dea5603ae88
Liz <gammon.jocular.0w@icloud.com>
2022-11-25 22:54:34 +0000

    removed unnecessary test

--- crates/common/src/api/mod.rs
+++ crates/common/src/api/mod.rs
@@ -19,24 +19,3 @@ pub enum Method<'a> {
 pub enum Response {
     Account(account::Response),
 }
-
-#[cfg(test)]
-mod test {
-    use super::*;
-
-    #[test]
-    fn test_message_transcode() -> Result<(), bincode::Error> {
-        let a = Message {
-            nonce: 1,
-            payload: vec![4u8, 3, 2, 1],
-        };
-
-        let data = bincode::serialize(&a)?;
-        let b: Message<&[u8]> = bincode::deserialize(&data)?;
-
-        assert_eq!(a.nonce, b.nonce);
-        assert_eq!(a.payload, b.payload);
-
-        Ok(())
-    }
-}
