ded1cffb198dda2556222446dc31d8fba5967be7
Liz <gammon.jocular.0w@icloud.com>
2023-05-04 00:37:46 +0100

    light colours & other style tweaks

--- packages/ui/src/routes/index.tsx
+++ packages/ui/src/routes/index.tsx
@@ -3,9 +3,14 @@ import "./index.scss";
 
 export default function Home() {
   return (
-    <main>
-      <A href="/login">Login</A>
-      <A href="/register">Create Account</A>
-    </main>
+    <>
+      <h1>Home</h1>
+      <p>
+        First of all, you can <A href="/login">login</A>
+      </p>
+      <p>
+        Otherwise, feel free to <A href="/register">create an account</A>
+      </p>
+    </>
   );
 }
