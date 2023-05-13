e41cd24d625f923fca532f6d1eecdae7866c2c12
Liz <gammon.jocular.0w@icloud.com>
2023-05-13 23:48:13 +0100

    hub dialog open/close

--- packages/ui/src/components/Hub.tsx
+++ packages/ui/src/components/Hub.tsx
@@ -1,5 +1,37 @@
+import { createSignal } from "solid-js";
+import styles from "./Hub.module.scss";
 import HubButton from "./HubButton";
 
 export default function Hub() {
-  return <HubButton onClick={() => console.log("clicked")} />;
+  const [dialog, setDialog] = createSignal<HTMLDialogElement>();
+  const [open, setOpen] = createSignal(false);
+  const [closing, setClosing] = createSignal(false);
+
+  return (
+    <>
+      <HubButton
+        onClick={() => {
+          dialog()?.showModal();
+          setOpen(true);
+        }}
+      />
+      <dialog
+        ref={setDialog}
+        classList={{
+          [styles.dialog]: true,
+          [styles.fadeOut]: closing(),
+        }}
+        onClick={() => {
+          setClosing(true);
+          setOpen(false);
+        }}
+        onAnimationEnd={() => {
+          if (!closing()) return;
+
+          dialog()?.close();
+          setClosing(false);
+        }}
+      ></dialog>
+    </>
+  );
 }
